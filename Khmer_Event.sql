6d60debf943ef804587ca885c50e2c11764d4d9e
SNsamnang <144607379+SNsamnang@users.noreply.github.com>
2023-11-22 23:06:49 +0700

    Add files via upload

---
+++ Khmer_Event.sql
@@ -0,0 +1,15 @@
+--create database Khmer_Event
+--create table tblKhmerEvent(
+--  EventID int IDENTITY(1,1) primary key,
+--  EventName nvarchar(200),
+--  Douration nvarchar(100),
+--  DateStart nvarchar(100),
+--  DateEnd nvarchar(100),
+--  Price money,
+--  Place nvarchar(100),
+--  [Description] nvarchar(max),
+--  Contace nvarchar(200),
+--  Link nvarchar(max),
+--  ImageURL nvarchar(200),
+--  Username nvarchar(200),
+--);
